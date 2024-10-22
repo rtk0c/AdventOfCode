23cc1ebbb5eaeb3a5a74b96a4e37d75b5599fc90
rtk0c <mail.tianyig@gmail.com>
2024-10-21 22:05:53 -0700

    really dumb solution

--- 2023/day01.hs
+++ 2023/day01.hs
@@ -10,15 +10,18 @@ firstlast s = read [T.head s, T.last s] :: Integer
 part1 :: [T.Text] -> Integer
 part1 = sum . map (firstlast . T.filter isDigit)
 
-wordToDigit = T.replace "one" "1"
-    . T.replace "two" "2"
-    . T.replace "three" "3"
+-- Out of all number words, starting letters are "otfsen", and ending letters are "eolrxnt"
+-- Only "eont" overlap, i.e. the order we do the replacement might matter if they involve words starting/ending with these
+-- Solution: just add an extra copy of the letter to allow more replacements
+wordToDigit = T.replace "one" "o1e"
+    . T.replace "two" "t2o"
+    . T.replace "three" "t3e"
     . T.replace "four" "4"
-    . T.replace "five" "5"
+    . T.replace "five" "5e"
     . T.replace "six" "6"
-    . T.replace "seven" "7"
-    . T.replace "eight" "8"
-    . T.replace "nine" "9"
+    . T.replace "seven" "7n"
+    . T.replace "eight" "e8t"
+    . T.replace "nine" "9e"
 
 part2 :: [T.Text] -> Integer
 part2 = part1 . map wordToDigit
