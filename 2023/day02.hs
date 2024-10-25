904bdb44a33902577dfc0114b406b82284616846
rtk0c <mail.tianyig@gmail.com>
2024-10-24 18:35:37 -0700

    better code

--- 2023/day02.hs
+++ 2023/day02.hs
@@ -1,21 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Use map once" #-}
-
+import Data.Char (isDigit)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Text.Read qualified as T
 
-dropBefore c = T.tail . snd . T.breakOn c
+decimal :: T.Text -> Int
+decimal = fst . either error id . T.decimal
 
-parse :: T.Text -> Int
-parse = fst . either error id . T.decimal
+map2 :: (a -> x) -> (b -> y) -> (a, b) -> (x, y)
+map2 f g (a, b) = (f a, g b)
 
 parseElm str = case T.split (== ' ') str of
-  n : "red" : _ -> (parse n, 0, 0)
-  n : "green" : _ -> (0, parse n, 0)
-  n : "blue" : _ -> (0, 0, parse n)
+  n : "red" : _ -> (decimal n, 0, 0)
+  n : "green" : _ -> (0, decimal n, 0)
+  n : "blue" : _ -> (0, 0, decimal n)
   _ -> (0, 0, 0)
 
 type Sample = (Int, Int, Int)
@@ -23,21 +22,30 @@ type Sample = (Int, Int, Int)
 merge :: Sample -> Sample -> Sample
 merge (a, b, c) (x, y, z) = (a + x, b + y, c + z)
 
+-- Turn "1 red, 2 blue, 3 green" into (1, 3, 2), aka Sample
 parseSample :: T.Text -> Sample
-parseSample =
-  foldl1 merge
-    . map (parseElm . T.strip)
-    . T.split (== ',')
+parseSample = foldl1 merge . map (parseElm . T.strip) . T.split (== ',')
 
-parseLine :: T.Text -> [Sample]
-parseLine = map (parseSample . T.strip) . T.split (== ';')
+-- Turn ';' separated samples into [Sample]
+parseSamples :: T.Text -> [Sample]
+parseSamples = map (parseSample . T.strip) . T.split (== ';')
 
--- https://stackoverflow.com/a/16192050
-mapInd :: (a -> b) -> [a] -> [(Int, b)]
-mapInd f = zipWith (\i a -> (i, f a)) [1 ..]
+-- Turn "Game NNN" into Int NNN
+parseIndex :: T.Text -> Int
+parseIndex = decimal . T.filter isDigit
+
+-- Same as Data.Text.breakOn on a char, but removes the separator char
+cutOn :: Char -> T.Text -> (T.Text, T.Text)
+cutOn c s =
+  let (front, back) = T.breakOn (T.singleton c) s
+   in (front, T.tail back)
 
 parseInput :: T.Text -> [(Int, [Sample])]
-parseInput = mapInd parseLine . map T.strip . map (dropBefore ":") . T.lines
+parseInput = map lf . T.lines
+  where
+    lf line =
+      let (game, samples) = cutOn ':' line
+       in (parseIndex game, parseSamples samples)
 
 legalSample (r, g, b) = r <= 12 && g <= 13 && b <= 14
 
