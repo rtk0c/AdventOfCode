3f33b9388869b941a967f0643620f964fb0a1714
rtk0c <mail.tianyig@gmail.com>
2024-12-01 10:21:44 -0800

    Day 1 parsing

---
+++ 2024/src/aoc2024/day01.clj
@@ -0,0 +1,21 @@
+(ns aoc2024.day01
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn parse-input []
+  ;; Bleh
+  (let [left-list (transient [])
+        right-list (transient [])]
+    (with-open [rdr (io/reader "inputs/day01.txt")]
+      (doseq [line (line-seq rdr)]
+        (let [left (subs line 0 (str/index-of line \space))
+              right (subs line (+ 1 (str/last-index-of line \space)))]
+          (conj! left-list (Integer/parseInt left))
+          (conj! right-list (Integer/parseInt right)))))
+    [(persistent! left-list) (persistent! right-list)]))
+
+(defn part1 [[llist rlist]])
+
+(defn solve []
+  (let [input (parse-input)]
+    (part1 input)))
--- 2024/src/aoc2024/main.clj
+++ 2024/src/aoc2024/main.clj
@@ -1,3 +1,4 @@
 (ns aoc2024.main)
 
-(defn main [])
+(defn main []
+  (aoc2024.day01/solve))
