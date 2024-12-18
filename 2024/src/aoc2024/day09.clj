593c5d66bf58857b6f5e6cc162c16238142c428c
rtk0c <mail.tianyig@gmail.com>
2024-12-18 15:22:20 -0800

    [2024] day 9 part 1
    Finally! The stupid parsing \n part took so long

--- 2024/src/aoc2024/day09.clj
+++ 2024/src/aoc2024/day09.clj
@@ -1,11 +1,12 @@
-(ns aoc2024.day09)
+(ns aoc2024.day09
+  (:require [clojure.string :as str]))
 (import aoc2024.Day09)
 
 (defn parse-input
   ([] (parse-input "inputs/day09.txt"))
   ([file]
    (map #(- (int %) (int \0))
-        (slurp file))))
+        (str/trim (slurp file)))))
 
 (defn part1 [input]
   (Day09/solvePart1 (int-array input)))
--- 2024/src/java/aoc2024/Day09.java
+++ 2024/src/java/aoc2024/Day09.java
@@ -3,82 +3,55 @@ package aoc2024;
 import java.util.*;
 
 public class Day09 {
-    private static Inode[] preprocess(int[] data) {
-        var input = new Inode[data.length];
-        int i = 0;
-        int fileId = 0;
-        boolean isFile = true;
-        for (int d : data) {
-            if (isFile)
-                input[i++] = new Inode(fileId++, d);
-            else
-                input[i++] = new Inode(-1, d);
-            isFile = !isFile;
+    private static int[] makeDiskMap(int[] input) {
+        int diskmapLen = 0;
+        for (int d : input) {
+            diskmapLen += d;
         }
-        return input;
+
+        var res = new int[diskmapLen];
+        int idx = 0;
+
+        boolean isFile = true;
+        int fileId = 0;
+        for (int d : input) {
+            int elm = isFile ? fileId++ : -1;
+            isFile = !isFile;
+            for (int i = 0; i < d; ++i)
+                res[idx++] = elm;
+        }
+
+        return res;
+    }
+
+    private static long checksum(int[] diskmap) {
+        long prod = 0;
+        for (int i = 0; i < diskmap.length; ++i) {
+            if (diskmap[i] == -1)
+                continue;
+            prod += i * diskmap[i];
+        }
+        return prod;
     }
 
     public static long solvePart1(int[] input) {
-        var out = new ArrayList<Inode>();
+        var disk = makeDiskMap(input);
+        int writep = 0;
+        int readp = disk.length - 1;
 
-        int copyPtr = 0;
-        int fillPtr = input.length - 1;
+        for (;;) {
+            while (disk[writep] != -1)
+                writep++;
+            while (disk[readp] == -1)
+                readp--;
 
-        while (copyPtr < fillPtr) {
-            out.add(new Inode(copyPtr / 2, input[copyPtr]));
-            int voidLen = input[copyPtr + 1];
+            if (writep >= readp)
+                break;
 
-            while (voidLen != 0) {
-                var fill = input[fillPtr];
-                if (voidLen >= fill) {
-                    out.add(new Inode(fillPtr / 2, fill));
-                    voidLen -= fill;
-                    //input[fillPtr] = 0;
-                    fillPtr -= 2;
-                } else {
-                    out.add(new Inode(fillPtr / 2, voidLen));
-                    input[fillPtr] -= voidLen;
-                    voidLen = 0;
-                }
-            }
-
-            copyPtr += 2;
-        }
-        if (copyPtr == fillPtr) {
-            out.add(new Inode(fillPtr / 2, input[fillPtr]));
+            disk[writep++] = disk[readp];
+            disk[readp--] = -1;
         }
 
-        long prod = 0;
-        int pos = 0;
-        for (Inode f : out) {
-            for (int i = 0; i < f.length; ++i) {
-                prod += (pos + i) * f.id;
-            }
-            pos += f.length;
-        }
-
-        return prod;
-    }
-}
-
-// Call it this because why not
-class Inode {
-    int id;
-    int length;
-
-    public Inode(int id, int length) {
-        this.id = id;
-        this.length = length;
-    }
-
-    // Mark empty space
-    public Inode(int length) {
-        this.id = -1;
-        this.length = length;
-    }
-
-    @Override
-    public String toString() {
-        return "{" + id + "}" + length;
+        return checksum(disk);
     }
 }
