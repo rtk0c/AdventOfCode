e7ec919cc2d5036707d30d2e4a90f4aa62d90497
rtk0c <mail.tianyig@gmail.com>
2024-12-20 13:17:18 -0800

    [2024] day 9 naughty inputs?

--- 2024/src/aoc2024/day09.clj
+++ 2024/src/aoc2024/day09.clj
@@ -13,8 +13,14 @@
   (Day09/solvePart1 (int-array input)))
 
 (defn part2 [input]
+  ;; TODO this doesn't work on the "naughty" input?
+  ;; https://old.reddit.com/r/adventofcode/comments/1haauty/2024_day_9_part_2_bonus_test_case_that_might_make/
+  ;;
+  ;; expected 97898222299196
+  ;; actual   21034411748353
   (Day09/solvePart2 (int-array input)))
 
 (defn solve []
   (let [input (parse-input)]
-    [(part1 input) (part2 input)]))
+    [(part1 input)
+     (part2 input)]))
